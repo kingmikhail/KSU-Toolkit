0cb901e8db78f93258910b30da041cf0d015e239
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-27 23:23:45 +0800

    change_uid: prepare for more usecase

--- src/change_uid.c
+++ src/change_uid.c
@@ -27,6 +27,33 @@ static int dumb_str_to_appuid(const char *str)
 	return uid;
 }
 
+/*
+ * strnmatch, test two strings if they match up to n len
+ *
+ * caller is reposnible for sanity! no \0 check!
+ * returns: 0 = match, 1 = not match
+ *
+ * Usage examples:
+ * for strcmp like behavior, strnmatch(x, y, strlen(y) + 1) (+1 for \0)
+ * for strstarts like behavior strnmatch(x, y, strlen(y))
+ *  
+ */
+static int strnmatch(const char *a, const char *b, unsigned int count)
+{
+	// og condition was like *a && (*a == *b) && count > 0
+	do {
+		// if they arent equal
+		if (*a != *b) 
+			return 1;
+		a++;
+		b++;
+		count --;
+	} while (count > 0);
+
+	// we reach here if they match
+	return 0;
+}
+
 static int fail(void)
 {
 	const char *error = "fail\n";
@@ -36,34 +63,33 @@ static int fail(void)
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		const char *error = "Usage: ./change_uid <uid>\n";
-		syscall(SYS_write, 2, error, strlen(error));
+	if (argc != 3) {
+		const char *usage = "Usage:\n./uidtool --setuid <uid>\n./uidtool --getuid";
+		syscall(SYS_write, 2, usage, strlen(usage));
 		return 1;
 	}
 	
 
-	int magic1 = 0xDEADBEEF;
-	int magic2 = 10006;
-	uintptr_t arg;
-	
-	unsigned int cmd = dumb_str_to_appuid(argv[1]);
-	if (cmd == 0)
-		return fail();
-	
-	syscall(SYS_reboot, magic1, magic2, cmd, (void *)&arg);
 
-#if 0 // we drop stdlib
-	printf("SYS_reboot(0x%x, %d, %u, %p)\n", magic1, magic2, cmd, (void *)&arg);
-	// if our arg contains our pointer then its good
-	printf("reply: 0x%lx verdict: %s\n", *(uintptr_t *)arg, *(uintptr_t *)arg == (uintptr_t)arg ? "ok" : "fail" );
-#endif
-	
-	if ( *(uintptr_t *)arg == arg )
-		syscall(SYS_write, 2, "ok\n", strlen("ok\n"));
-	else
+	if (!strnmatch(argv[1], "--setuid", strlen("--setuid") + 1)) {
+		int magic1 = 0xDEADBEEF;
+		int magic2 = 10006;
+		uintptr_t arg;
+		
+		unsigned int cmd = dumb_str_to_appuid(argv[2]);
+		if (cmd == 0)
+			return fail();
+		
+		syscall(SYS_reboot, magic1, magic2, cmd, (void *)&arg);
+
+		if ( *(uintptr_t *)arg == arg ) {
+			syscall(SYS_write, 2, "ok\n", strlen("ok\n"));
+			return 0;
+		}
+		
 		return fail();
-	
+	}
+
 	return 0;
 
 }
