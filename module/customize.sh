0ec9289ab6eafda7f2a39efeec6ae9cc0c042e36
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-11-29 14:38:04 +0800

    scripts/customize: pre-check interface availability

--- module/customize.sh
+++ module/customize.sh
@@ -1,1 +1,11 @@
+#!/bin/sh
+
 chmod 755 "$MODPATH/uid_tool"
+
+current_uid=$("$MODPATH/uid_tool" --getuid)
+
+if ! "$MODPATH/uid_tool" --setuid "$current_uid" >/dev/null 2>&1; then
+	abort "[!] custom interface not available!"
+fi
+
+# EOF
