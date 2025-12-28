1ec0ee5e6d38b68103abd0b4d4d16215af52f512
KOWX712 <leecc0503@gmail.com>
2025-12-28 18:05:12 +0800

    scripts/uninstall: remove version file

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -5,6 +5,6 @@
 # This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
 KSUDIR="/data/adb/ksu"
 
-rm -rf "$KSUDIR/.manager_uid" "$KSUDIR/.umount_list"
+rm -rf "$KSUDIR/.manager_uid" "$KSUDIR/.manager_version" "$KSUDIR/.umount_list"
 
 rm "$KSUDIR/bin/toolkit"
