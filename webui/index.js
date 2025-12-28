5dd5892e40aaa7f793594808af9fe9ed40cb5d18
KOWX712 <leecc0503@gmail.com>
2025-12-28 16:32:51 +0800

    webui: add back button on supported platform

--- webui/index.js
+++ webui/index.js
@@ -380,6 +380,16 @@ function checkUpdate() {
 document.addEventListener('DOMContentLoaded', async () => {
     document.querySelectorAll('[unresolved]').forEach(el => el.removeAttribute('unresolved'));
 
+    // exit button
+    const exitBtn = document.getElementById('exit-btn');
+    if (typeof window.ksu !== 'undefined' && typeof window.ksu.exit !== 'undefined') {
+        exitBtn.onclick = () => ksu.exit()
+    } else if (typeof window.webui !== 'undefined' && typeof window.webui.exit !== 'undefined') {
+        exitBtn.onclick = () => webui.exit()
+    } else {
+        exitBtn.style.display = 'none';
+    }
+
     // tab init
     const mdTab = document.querySelector('md-tabs');
     mdTab.addEventListener('change', async () => {
